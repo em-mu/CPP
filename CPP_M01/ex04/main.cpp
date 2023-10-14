1c985f9c328d887f3439590c3041d6502d05af84
Emma Muller <emuller@k0r2p7.42mulhouse.fr>
2023-10-14 16:57:52 +0200

    module 1 partially fixed 2

--- CPP_M01/ex04/main.cpp
+++ CPP_M01/ex04/main.cpp
@@ -6,13 +6,29 @@
 /*   By: emuller <emuller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 20:07:08 by emuller           #+#    #+#             */
-/*   Updated: 2023/10/11 16:04:21 by emuller          ###   ########.fr       */
+/*   Updated: 2023/10/14 16:55:40 by emuller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include <fstream>
+
+std::string    my_replace(std::string line, std::string s1, std::string s2)
+{
+	size_t	pos_search = 0;
+
+	pos_search = line.find(s1);
+	while (pos_search != std::string::npos)
+	{
+		line.erase(pos_search, s1.length());
+		line.insert(pos_search, s2);
+		pos_search = line.find(s1, pos_search + s2.length());
+	}
+	return (line);
+}
+
+
 
 int main(int argc, char **argv)
 {   
@@ -31,7 +47,7 @@ int main(int argc, char **argv)
     infile.open(argv[1]);
     if (infile.fail())
     {
-        std::cout << "\033[1;31;5;221mError: " << argv[1] << ":" << " no such file or directory\033[0m" << std::endl;
+        std::cout << "\033[1;31;5;221mError: " << argv[1] << ":" << "\033[0m" << std::endl;
         return 1;
     }
 
@@ -41,6 +57,15 @@ int main(int argc, char **argv)
     while (!infile.eof() && infile >> std::noskipws >> c)
         content += c;
 
+				// while (std::getline(infile, buffer))
+				// {
+				// 	buffer = my_replace(buffer, search, replace);
+				// 	outfile << buffer;
+				// 	if (infile.peek() != EOF)
+				// 		outfile << std::endl;
+				// }
+
+                
     // Create an output file and fill it
     outfile.open((filename + ".replace").c_str());
     if (outfile.fail())
