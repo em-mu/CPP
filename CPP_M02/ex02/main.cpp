5c22d79f15441a93f647e30d1f9a64b8151d194c
emuller <emuller@Asus>
2023-10-18 14:39:51 +0200

    more tests

--- CPP_M02/ex02/main.cpp
+++ CPP_M02/ex02/main.cpp
@@ -23,5 +23,15 @@ int main()
     std::cout << a << std::endl;
     std::cout << b << std::endl;
     std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << "a+b:  " << a + b << std::endl; 
+    std::cout << "a-b:  " << a - b << std::endl; 
+    std::cout << "a*b:  " << a * b << std::endl; 
+    std::cout << "a/b:  " << a / b << std::endl; 
+    if (a != b)
+        std::cout << "a != b:  " << std::endl; 
+    if (a > b)
+        std::cout << "a > b:  " << std::endl;    
+    if (a < b)
+        std::cout << "a < b:  " << std::endl;
     return 0;
 }
